283cf385fd98383f93902267eb3e077a924092c1
Alicia Ong <alicia.ong@aliciaong-ltm4.internal.salesforce.com>
2014-11-03 15:52:19 -0800

    Dynamic select box

--- src/main/webapp/wedding.js
+++ src/main/webapp/wedding.js
@@ -1,4 +1,6 @@
+var curGuests = -1;
 var numGuestsAllowed = -1;
+var numGuests = -1;
 var names = '';
 var showRsvp = function(){
     toggle(false, true, false, false);
@@ -17,11 +19,28 @@ var toggle = function(landing, rsvp, info, contact){
     setClass("info", info);
 }
 var submitRsvp = function(){
-    alert('submit');
+    var req = new XMLHttpRequest();
+    req.open("get", "/rsvp?code=" +document.getElementById('code').value + "&numGuests=" + numGuests, true);
+    req.onreadystatechange = function(){
+        if (req.readyState == 4){
+            if (req.status == 200){
+                var response = JSON.parse(req.response);
+                if (response["errorCode"]){
+                    alert(response["details"]);
+                }else{
+                    //do something
+                }
+            }else{
+                alert('Could not rsvp');
+            }
+        }
+    }
+    req.send();
+
 }
 var submitCode = function(){
     var req = new XMLHttpRequest();
-    req.open("get", "/rsvp?code=" +document.getElementById('code').value, true);
+    req.open("get", "/login?code=" +document.getElementById('code').value, true);
     //    req.open("post", "/rsvp", true);
     //    var params = "code=" + document.getElementById("code").value;
     req.onreadystatechange = function(){
@@ -34,6 +53,11 @@ var submitCode = function(){
                     names = response.result["name"];
                     document.getElementById("names").innerHTML = names;
                     numGuestsAllowed = response.result["maxGuests"];
+                    alterPicklist(numGuestsAllowed);
+                    curGuests = response.result["actualGuests"];
+                    if (curGuests != null){
+                        document.getElementById("curGuests").innerHTML = "You currently have " + curGuests + " spot(s) reserved";
+                    }
                     showInfo();
                 }
             }else{
@@ -43,6 +67,21 @@ var submitCode = function(){
     }
     req.send();
     //    req.send(params);
+}
+var alterPicklist = function(maxGuests){
+    if (maxGuests > 1){
+        var newPicklist = " and <select id=\"numGuests\">";
+        for (var i = 0 ; i < maxGuests; i++){ //2 max guests, then 0, 1
+            var displayValue = i;
+            if (i == 0){
+                displayValue = "no"
+            }
+            newPicklist += "<option value=\"" + i + "\">" + displayValue + "</option>";
+        }
+        newPicklist += "</select>";
+        newPicklist += (maxGuests == 2 ? "other guest" : "other guests");
+        document.getElementById("guestSelector").innerHTML = newPicklist;
+    }
 }
 var setClass = function(elemName, value){
     var elem = document.getElementById(elemName);
